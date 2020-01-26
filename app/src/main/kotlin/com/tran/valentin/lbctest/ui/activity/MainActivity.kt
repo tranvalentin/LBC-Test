c5f50dc965accf6dd0025ab0e8fd4782673c72cd
Valentin Tran <vtran@wynd.eu>
2020-01-26 11:41:38 +0100

    add splash activity

---
+++ app/src/main/kotlin/com/tran/valentin/lbctest/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tran.valentin.lbctest.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tran.valentin.lbctest.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/kotlin/com/tran/valentin/lbctest/ui/activity/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.tran.valentin.lbctest.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.tran.valentin.lbctest.R
+import com.tran.valentin.lbctest.service.manager.ManagerLocator
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class SplashActivity : AppCompatActivity() {
+
+    private val loader by lazy { findViewById<View>(R.id.loader) }
+    private val rootView by lazy { findViewById<View>(R.id.rootView) }
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        compositeDisposable.add(
+            ManagerLocator.findLBCManager().getAlbums().subscribeOn(Schedulers.io()).observeOn(
+                AndroidSchedulers.mainThread()
+            ).subscribe({ launchMainActivity() }, {
+                loader.visibility = View.GONE
+                Snackbar.make(rootView, getString(R.string.error_splash), Snackbar.LENGTH_LONG)
+                    .show()
+            })
+        )
+    }
+
+    private fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
